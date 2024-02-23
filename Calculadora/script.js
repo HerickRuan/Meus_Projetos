6c8837ca4bc52fd2aa2060ddd6bd31f5a11db9d2
Herick Ruan <152439267+HerickRuan@users.noreply.github.com>
2024-02-23 15:31:11 -0300

    update

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -5,13 +5,18 @@ var val2t = document.querySelector('#ival2')
 function porcent(){
     var val1 = Number(val1t.value)
     var val2 = Number(val2t.value) 
-
-    if ((val1 || val2) <= 0){
-        res.innerHTML = `<p>Só valor positivo fazendo o favor.</p><p>Só valor positivo fazendo o favor.</p><p>Só valor positivo fazendo o favor.</p><p>Só valor positivo fazendo o favor.</p>`
-    } else {
-        
-
     res.innerHTML = ''
-    res.innerHTML += `${val1 + val2} `
+      
+    if ((val1 || val2) <= 0){
+        res.innerHTML = `<p>Só valor positivo fazendo o favor.</p>`
+    } else {
+        var array = []
+        array.push(Number(val1))
+        array.push(Number(val2))
+        array.sort()//Assim, sempre, na posição "0" estará o menor e no "1" o maior.
+
+        var difer = 100 - ((array[0] * 100) / array[1])//Usei regra de tres aq
+        res.innerHTML += `Diferença de: <strong>${difer.toFixed(1)}%</strong>`
+
     }
 }
