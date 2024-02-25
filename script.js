86c051640a64ee23a57da3531363056fbad71de6
Herick Ruan <152439267+HerickRuan@users.noreply.github.com>
2024-02-25 15:27:43 -0300

    aqui ta bom

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 var altt = document.querySelector('#ialt')
 var pesot = document.querySelector('#ipeso')
 var idadet = document.querySelector('#iidade')
+var imagens = document.querySelector('#imagens')
+var section = document.querySelector('#sec2')
+var main2 = document.querySelector('#main2')
+//Imagens
+var abaixo = 'Imagens/abaixo.png'
+var normal = 'Imagens/normal.png'
+var sobrepes = 'Imagens/sobrepes.png'
+var obes1 = 'Imagens/obes-1.png'
+var obes2 = 'Imagens/obes-2.png'
+var obes3 = 'Imagens/obes-3.png'
+
 
 
 function saude(){
@@ -10,6 +21,7 @@ function saude(){
     var idade = Number(idadet.value)
     res2.style.display = "block" 
     res2.innerHTML = ''
+
 
     if((alt || peso) <= 0 || idade > 120){
         res2.innerHTML +=`Insira um valor válido.`
@@ -24,21 +36,38 @@ function saude(){
         if(imc >= 40 ){
             resultado = 'Obesidade Grau III'
             res2.innerHTML += `O peso ideal seria entre <span class="verm">${pesoi1.toFixed(1)} Kg</span> e <span class="verm">${pesoi2.toFixed(1)} Kg</span>.`
+
+            imagens.style.backgroundImage = 'url('+obes3+')'
+
         } else if (imc >= 35){
             resultado = 'Obesidade Grau II'
             res2.innerHTML += `O peso ideal seria entre <span class="verm">${pesoi1.toFixed(1)} Kg</span> e <span class="verm">${pesoi2.toFixed(1)} Kg</span>.`
+
+            imagens.style.backgroundImage = 'url('+obes2+')'
+
         } else if (imc >= 30){
             resultado = 'Obesidade Grau I'
             res2.innerHTML += `O peso ideal seria entre <span class="verm">${pesoi1.toFixed(1)} Kg</span> e <span class="verm">${pesoi2.toFixed(1)} Kg</span>.`
+
+            imagens.style.backgroundImage = 'url('+obes1+')'
+
         } else if (imc >= 25){
             resultado = 'Sobrepeso'
             res2.innerHTML += `O peso ideal seria entre <span class="verm">${pesoi1.toFixed(1)} Kg</span> e <span class="verm">${pesoi2.toFixed(1)} Kg</span>.`
+
+            imagens.style.backgroundImage = 'url('+sobrepes+')'
+
         } else if (imc >= 18.5){
             resultado = 'Peso Normal'
+
+            imagens.style.backgroundImage = 'url('+normal+')'
 
         } else if (imc < 18.5){
             resultado = 'Baixo Peso'
             res2.innerHTML += `<p>O peso ideal seria entre <span class="verm">${pesoi1.toFixed(1)}Kg</span> e <span class="verm">${pesoi2.toFixed(1)}Kg</span>.</p>`
+
+            imagens.style.backgroundImage = 'url('+abaixo+')'
+
         }
 
         res2.innerHTML += `<p id="p_res2">Seu <a href="https://pt.wikipedia.org/wiki/Índice_de_massa_corporal" target="_blank">IMC</a> é: ${imc.toFixed(1)} - ${resultado}</p>`
@@ -61,6 +90,14 @@ function saude(){
             res2.innerHTML += `<p>Você deve beber <strong>${aguaJ.toFixed(1)}</strong> litros de água por dia.</p>`
         }
         
+        //Posicionar itens 
+        imagens.style.display = "block"
+        section.style.float = "left"
+        section.style.marginRight = "10px"
+        main2.style.height = "420px"
+
+
+
     }
 
 }
